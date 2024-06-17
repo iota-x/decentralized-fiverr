9062b6e142a3467be1cc104c0a6ea24af75fff57
iota <ishu2000pandey@gmail.com>
2024-06-18 02:30:19 +0530

    completed presignedUrls backend

--- backend/dist/routers/user.js
+++ backend/dist/routers/user.js
@@ -15,9 +15,35 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const client_1 = require("@prisma/client");
-const JWT_SECRET = "ankit20";
+const client_s3_1 = require("@aws-sdk/client-s3");
+const s3_request_presigner_1 = require("@aws-sdk/s3-request-presigner");
+const __1 = require("..");
+const middleware_1 = require("../middleware");
+const s3Client = new client_s3_1.S3Client({
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+    region: "ap-south-1"
+});
 const router = (0, express_1.Router)();
 const prismaClient = new client_1.PrismaClient();
+router.get("/presignedUrl", middleware_1.authMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    // @ts-ignore
+    const userId = req.userId;
+    const command = new client_s3_1.PutObjectCommand({
+        Bucket: "my-decentralized-fiverr",
+        Key: `/fiverr/${userId}/${Math.random()}/image.jpg`,
+        ContentType: "img/jpg"
+    });
+    const preSignedUrl = yield (0, s3_request_presigner_1.getSignedUrl)(s3Client, command, {
+        expiresIn: 3600
+    });
+    console.log(preSignedUrl);
+    res.json({
+        preSignedUrl
+    });
+}));
 router.post("/signin", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const hardcodedWalletAddress = "M5vrwrH8rv54Ryt3NqwSA4Fj16vmdKieiM2oYqgSgpu";
     const existingUser = yield prismaClient.user.findFirst({
@@ -28,7 +54,7 @@ router.post("/signin", (req, res) => __awaiter(void 0, void 0, void 0, function*
     if (existingUser) {
         const token = jsonwebtoken_1.default.sign({
             userId: existingUser.id
-        }, JWT_SECRET);
+        }, __1.JWT_SECRET);
         res.json({
             token: token
         });
@@ -41,7 +67,7 @@ router.post("/signin", (req, res) => __awaiter(void 0, void 0, void 0, function*
         });
         const token = jsonwebtoken_1.default.sign({
             userId: user.id
-        }, JWT_SECRET);
+        }, __1.JWT_SECRET);
         res.json({
             token: token
         });
--- backend/src/routers/user.ts
+++ backend/src/routers/user.ts
@@ -1,27 +1,42 @@
 import { Router } from "express";
 import jwt from 'jsonwebtoken';
 import { PrismaClient } from "@prisma/client";
-import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { JWT_SECRET } from "..";
 import { authMiddleware } from "../middleware";
+import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
 
-const s3Client = new S3Client()
+const s3Client = new S3Client({
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+    region: "ap-south-1"
+})
 
 const router = Router();
 const prismaClient = new PrismaClient();
 
-router.get("/presignedUrl", authMiddleware, (req, res) => {
+router.get("/presignedUrl", authMiddleware, async (req, res) => {
     // @ts-ignore
     const userId = req.userId;
 
-    const command = new PutObjectCommand({
+    const {url, fields } = await createPresignedPost(s3Client, {
         Bucket: "my-decentralized-fiverr",
-        Key: `/fiverr/${}`
+        Key: `/fiverr/${userId}/${Math.random()}/image.jpg`,
+        Conditions: [
+            ['content-length-range', 0, 5 * 1024 * 1024] // 5mb max
+        ], 
+        Fields: {
+            'Content-Type': 'image/png'
+        },
+        Expires: 3600
     })
 
-    const preSignedUrl = await getSignedUrl(s3Client, command, {
-        expiresIn: 3600
+    res.json({
+        preSignedUrl: url,
+        fields
     })
 
 })
